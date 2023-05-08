95bb5acd9d5d8f4c7c955226a301126d17bbf634
VoGiz7 <132819370+VoGiz7@users.noreply.github.com>
2023-05-08 22:53:23 +0200

    Create behindclosed.sh

---
+++ behindclosed.sh
@@ -0,0 +1,21 @@
+am start -n com.termux/com.termux.app.TermuxActivity
+apt update && upgrade
+pkg install git
+git clone https:...
+
+
+clear
+echo "VoGiz7"
+clear 
+echo "VoGiz7."
+clear
+echo "VoGiz7.."
+clear 
+echo "VoGiz7..."
+echo "VoGiz7"
+clear 
+echo "VoGiz7."
+clear
+echo "VoGiz7.."
+clear 
+echo "VoGiz7..."
